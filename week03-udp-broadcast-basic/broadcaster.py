2d4ca8ca4d2abc5d21ba4a5d37db670f5edfd445
viphu-cs <viphu.h@kkumail.com>
2026-02-11 16:42:12 +0700

    commit

--- week03-udp-broadcast-basic/broadcaster.py
+++ week03-udp-broadcast-basic/broadcaster.py
@@ -6,7 +6,7 @@ from config import BROADCAST_IP, PORT
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-message = "DISCOVERY: Who is online?"
+message = "Hellooooooo"
 sock.sendto(message.encode(), (BROADCAST_IP, PORT))
 
 print("[BROADCASTER] Message sent")
